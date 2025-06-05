ce527a2958468e1fe2f56234a2f2109c4dbe44f1
praisen <65597209+praisen@users.noreply.github.com>
2025-06-05 12:36:09 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public\Rajakumar_Sekar_CV.pdf.pdf.pdf"
+                href="/Rajakumar_Sekar_CV.pdf.pdf.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
